5b70fd320e6a5162fe1f6d59f13dd0cac3b9239b
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 10:44:00 +0100

    sendtoqueu

--- HistoryProducer/src/main/java/com/bae/rest/HistoryController.java
+++ HistoryProducer/src/main/java/com/bae/rest/HistoryController.java
@@ -1,8 +1,23 @@
 package com.bae.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.domain.History;
+import com.bae.domain.SentHistory;
 
 @RestController
 public class HistoryController {
+	
+	private JmsTemplate jmsTemplate;
 
+	public HistoryController(@Autowired JmsTemplate jmsTemplate) {
+		this.jmsTemplate = jmsTemplate;
+	}
+	
+	private void sentToQueue(History history) {
+		SentHistory historyToStore = new SentHistory(history);
+		jmsTemplate.convertAndSend("HistoryQueue", historyToStore);
+	}
 }
