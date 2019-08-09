3fa7036eb6d8c2d076ff9e1bb632955ac19deb96
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 12:52:27 +0100

    userrepository

---
+++ User/src/main/java/com/bae/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bae.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bae.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
