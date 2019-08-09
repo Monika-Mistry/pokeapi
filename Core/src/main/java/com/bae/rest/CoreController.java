d8acbdf220141a91b8c969431defe84b31b40271
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 13:57:00 +0100

    refactored user controller

--- Core/src/main/java/com/bae/rest/CoreController.java
+++ Core/src/main/java/com/bae/rest/CoreController.java
@@ -1,38 +1,43 @@
 package com.bae.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.bae.domain.User;
-import com.bae.service.UserService;
 
 @RestController
-public class UserController {
+public class CoreController {
+
+	@Value("${url.userURL}")
+	private String userURL;
 	
-	private UserService service;
+	@Value("${path.userExists}")
+	private String userExistsPath;
+	
 	private RestTemplate restTemplate;
-	
-	public UserController(@Autowired UserService service, @Autowired RestTemplate restTemplate) {
-		this.service = service;
+
+	public CoreController(@Autowired RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
-	
-	@GetMapping("/{userId}")
-	public ResponseEntity<Boolean> doesUserExist(@PathVariable("userId") Long id) {
-		return new ResponseEntity<Boolean>(service.userIdExists(id), HttpStatus.OK);
-	}
-	
 	@PostMapping
 	public ResponseEntity<User> createUser(@RequestBody User user) {
-		return new ResponseEntity<User>(service.createUser(user), HttpStatus.CREATED);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+
 	}
+
+	@GetMapping("/{userId}")
+	public boolean doesUserExist(@PathVariable("userId") Long id) {
+		return restTemplate.exchange(userURL + userExistsPath + id, HttpMethod.GET, null, boolean.class);
+	}
+
 }
--- User/src/test/java/com/bae/TestConstants.java
+++ User/src/test/java/com/bae/TestConstants.java
@@ -13,5 +13,7 @@ public class TestConstants {
 	public static final Optional<User> MOCK_USER_OPTIONAL = Optional.of(new User(1L, "1", "John"));
 	public static final Optional<User> MOCK_NULL_OPTIONAL = Optional.empty();
 	public static final ResponseEntity<User> MOCK_USER_RESPONSE = new ResponseEntity<User>(MOCK_USER, HttpStatus.CREATED);
+	public static final ResponseEntity<Boolean> MOCK_USER_EXISTS = new ResponseEntity<Boolean>(true, HttpStatus.OK);
+	public static final ResponseEntity<Boolean> MOCK_USER_NOT_EXISTS = new ResponseEntity<Boolean>(false, HttpStatus.OK);
 
 }
