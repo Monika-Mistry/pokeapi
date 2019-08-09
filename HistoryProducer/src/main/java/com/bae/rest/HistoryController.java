75481bf933096db68b0ee4d967dc10a9e06d61f0
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 10:55:58 +0100

    controller get method

--- HistoryProducer/src/main/java/com/bae/rest/HistoryController.java
+++ HistoryProducer/src/main/java/com/bae/rest/HistoryController.java
@@ -1,8 +1,12 @@
 package com.bae.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.bae.domain.History;
 import com.bae.domain.SentHistory;
@@ -10,13 +14,31 @@ import com.bae.domain.SentHistory;
 @RestController
 public class HistoryController {
 	
+	@Value("${url.user}")
+	private String userURL;
+	
+	@Value("${path.getHistory}")
+	private String getHistoryPath;
+	
 	private JmsTemplate jmsTemplate;
+	private RestTemplate restTemplate;
 
-	public HistoryController(@Autowired JmsTemplate jmsTemplate) {
+	public HistoryController(@Autowired JmsTemplate jmsTemplate, @Autowired RestTemplate restTemplate) {
 		this.jmsTemplate = jmsTemplate;
+		this.restTemplate = restTemplate;
 	}
 	
-	private void sentToQueue(History history) {
+	@GetMapping
+	public ResponseEntity<History> getHistory() {
+		ResponseEntity<History> history = restTemplate.getForEntity(userURL + getHistoryPath, History.class);
+		
+		sendToQueue(history.getBody());
+		
+		return history;
+	}
+	
+	
+	private void sendToQueue(History history) {
 		SentHistory historyToStore = new SentHistory(history);
 		jmsTemplate.convertAndSend("HistoryQueue", historyToStore);
 	}
