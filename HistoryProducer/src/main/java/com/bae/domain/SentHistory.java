c9c0443541e7ee0329c264d673fb36799b9fa550
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 10:34:51 +0100

    history

---
+++ HistoryProducer/src/main/java/com/bae/domain/SentHistory.java
@@ -0,0 +1,57 @@
+package com.bae.domain;
+
+import java.sql.Timestamp;
+
+public class SentHistory {
+	
+	private Long id;
+	
+	private String memberNumber;
+	
+	private String name;
+	
+	private String searchTerm;
+	
+	private Timestamp timestamp;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMemberNumber() {
+		return memberNumber;
+	}
+
+	public void setMemberNumber(String memberNumber) {
+		this.memberNumber = memberNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+	}
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}	
+
+}
