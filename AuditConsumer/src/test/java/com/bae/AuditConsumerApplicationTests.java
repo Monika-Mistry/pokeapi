986ef7a5f8baabe645dc91c505165f62b43b57cc
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 10:25:17 +0100

    smoke test added

--- AuditConsumer/src/test/java/com/bae/AuditConsumerApplicationTests.java
+++ AuditConsumer/src/test/java/com/bae/AuditConsumerApplicationTests.java
@@ -1,16 +1,25 @@
 package com.bae;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bae.service.HistoryService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuditConsumerApplicationTests {
+	
+	@Autowired
+	private HistoryService service;
 
 	@Test
 	public void contextLoads() {
+		assertThat(service).isNotNull();
 	}
 
 }
