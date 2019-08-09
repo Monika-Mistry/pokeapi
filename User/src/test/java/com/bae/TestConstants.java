af376f38d51b784ad2ec827568ffe7279a8f985d
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 13:21:22 +0100

    service tests

--- User/src/test/java/com/bae/TestConstants.java
+++ User/src/test/java/com/bae/TestConstants.java
@@ -1,9 +1,13 @@
 package com.bae;
 
+import java.util.Optional;
+
 import com.bae.domain.User;
 
 public class TestConstants {
 	
 	public static final User MOCK_USER = new User(1L, "1", "John");
+	public static final Optional<User> MOCK_USER_OPTIONAL = Optional.of(new User(1L, "1", "John"));
+	public static final Optional<User> MOCK_NULL_OPTIONAL = Optional.empty();
 
 }
