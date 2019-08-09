0c10f09857bf0418a7f902e76b26d3a5854e34d0
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 09:59:36 +0100

    app-config

---
+++ AuditConsumer/src/main/java/com/bae/AppConfig.java
@@ -0,0 +1,36 @@
+package com.bae;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	   public JmsListenerContainerFactory<?> jmsFactory(ConnectionFactory connectionFactory,
+	     DefaultJmsListenerContainerFactoryConfigurer configurer) {
+	       DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+	       configurer.configure(factory, connectionFactory);
+	       return factory;
+	   }
+	    
+	    @Bean
+	    public MessageConverter jacksonJmsMessageConverter() {
+	        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+	        converter.setTargetType(MessageType.TEXT);
+	        converter.setTypeIdPropertyName("_type");
+	        return converter;
+	    }
+
+}
