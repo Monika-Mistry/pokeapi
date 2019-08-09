533665dd184b5bb7d7f53aa5de5283af5a8c5f6d
Monika-Mistry <monika.mistry1@gmail.com>
2019-08-09 10:45:01 +0100

    resttemplate bean added

--- HistoryProducer/src/main/java/com/bae/AppConfig.java
+++ HistoryProducer/src/main/java/com/bae/AppConfig.java
@@ -1,5 +1,6 @@
 package com.bae;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -9,6 +10,12 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class AppConfig {
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
 	@Bean
 	public MessageConverter jacksonJmsMessageConverter() {
 		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
